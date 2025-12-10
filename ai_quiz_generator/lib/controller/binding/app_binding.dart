769dcc14bf5988d66b08e34d29c36073fc5b9e9d
Kyran-Git <niklauscareer@gmail.com>
2025-12-11 01:06:36 +0800

    Add DifficultyLevel and QuestionType enums with mappers for improved quiz functionality

--- ai_quiz_generator/lib/controller/binding/app_binding.dart
+++ ai_quiz_generator/lib/controller/binding/app_binding.dart
@@ -1,9 +1,11 @@
-import 'package:get/get.dart';
 import 'package:ai_quiz_generator/controller/ai_controller.dart';
+import 'package:ai_quiz_generator/data/services/quiz_generator_service.dart';
+import 'package:get/get.dart';
 
 class AppBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put<AiController>(AiController());
+    Get.put<QuizGeneratorService>(MockQuizGeneratorService());
+    Get.put<AiController>(AiController(quizGeneratorService: Get.find()));
   }
 }
