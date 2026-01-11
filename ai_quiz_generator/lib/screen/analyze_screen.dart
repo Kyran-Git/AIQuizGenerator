8545ecd0de10e7af5a1dae328c3917f64677696b
2025300567-glitch <2025300567@student.uitm.edu.my>
2026-01-11 15:22:48 +0800

    Add download button and refactor and simplify question grouping logic

--- ai_quiz_generator/lib/screen/analyze_screen.dart
+++ ai_quiz_generator/lib/screen/analyze_screen.dart
@@ -1,6 +1,8 @@
 import 'package:ai_quiz_generator/controller/ai_controller.dart';
 import 'package:ai_quiz_generator/data/models/quiz_question.dart';
+import 'package:ai_quiz_generator/data/models/quiz.dart'; 
 import 'package:ai_quiz_generator/theme/app_theme.dart';
+import 'package:ai_quiz_generator/screen/pdf_preview_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,18 +12,6 @@ class AnalyzeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AiController controller = Get.find();
-
-    // Group Questions by Topic
-    final Map<String, List<Question>> questionsByTopic = {};
-
-    for (var quiz in controller.myLibrary) {
-      final topic = quiz.title.isNotEmpty ? quiz.title : "General Review";
-      
-      if (!questionsByTopic.containsKey(topic)) {
-        questionsByTopic[topic] = [];
-      }
-      questionsByTopic[topic]!.addAll(quiz.questions);
-    }
 
     return Scaffold(
       backgroundColor: const Color(0xFFF5F7FA),
@@ -34,17 +24,22 @@ class AnalyzeScreen extends StatelessWidget {
         elevation: 0,
         iconTheme: const IconThemeData(color: Color(0xFF2C3E50)),
       ),
-      body: questionsByTopic.isEmpty
-          ? _buildEmptyState()
-          : ListView.builder(
-              padding: const EdgeInsets.all(16),
-              itemCount: questionsByTopic.keys.length,
-              itemBuilder: (context, index) {
-                final topic = questionsByTopic.keys.elementAt(index);
-                final questions = questionsByTopic[topic]!;
-                return _TopicCard(topic: topic, questions: questions);
-              },
-            ),
+      // Use Obx here to listen to changes in the library
+      body: Obx(() {
+        if (controller.myLibrary.isEmpty) {
+          return _buildEmptyState();
+        }
+
+        return ListView.builder(
+          padding: const EdgeInsets.all(16),
+          itemCount: controller.myLibrary.length,
+          itemBuilder: (context, index) {
+            final quiz = controller.myLibrary[index];
+            final topic = quiz.title.isNotEmpty ? quiz.title : "General Review";
+            return _TopicCard(topic: topic, quiz: quiz);
+          },
+        );
+      }),
     );
   }
 
@@ -67,9 +62,9 @@ class AnalyzeScreen extends StatelessWidget {
 
 class _TopicCard extends StatelessWidget {
   final String topic;
-  final List<Question> questions;
+  final dynamic quiz; // Using dynamic here avoids the import error if the class name is slightly different
 
-  const _TopicCard({required this.topic, required this.questions});
+  const _TopicCard({required this.topic, required this.quiz});
 
   @override
   Widget build(BuildContext context) {
@@ -87,15 +82,22 @@ class _TopicCard extends StatelessWidget {
           topic,
           style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
         ),
-        subtitle: Text("${questions.length} Questions"),
+        subtitle: Text("${quiz.questions.length} Questions"),
+        trailing: IconButton(
+          tooltip: 'Download PDF',
+          icon: const Icon(Icons.download_rounded, color: Color(0xFF4169E1)),
+          onPressed: () {
+            Get.to(() => PdfPreviewScreen(quiz: quiz, isRevisionMode: true));
+          },
+        ),
         children: [
           ListView.separated(
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            itemCount: questions.length,
+            itemCount: quiz.questions.length,
             separatorBuilder: (_, __) => const Divider(height: 1),
             itemBuilder: (context, index) {
-              final q = questions[index];
+              final q = quiz.questions[index];
               return Container(
                 padding: const EdgeInsets.all(16),
                 child: Column(
@@ -122,10 +124,9 @@ class _TopicCard extends StatelessWidget {
                             child: Text(
                               q.correctAnswer,
                               style: const TextStyle(
-                                fontSize: 13, 
-                                fontWeight: FontWeight.bold, 
-                                color: Colors.green
-                              ),
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.green),
                             ),
                           ),
                         ],
